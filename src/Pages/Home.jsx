b10925e9dab16807ad5063683e13cdb88f05f235
Dushimepaulette1 <p.dushime1@alustudent.com>
2025-08-04 13:28:23 +0200

    added pages and styled them

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Vans from "./Vans";
 export default function Home() {
   return (
     <div className="home-container">
@@ -8,7 +9,7 @@ export default function Home() {
         Add adventure to your life by joining the #vanlife movement. Rent the
         perfect van to make your perfect road trip.
       </p>
-      <Link to="vans">Find your van</Link>
+      <Link to="/Vans">Find your van</Link>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import "./index.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home.jsx";
 import About from "./Pages/About.jsx";
+import Vans from "./Pages/Vans.jsx";
+import "../server";
 function App() {
   return (
     <BrowserRouter>
@@ -12,11 +14,13 @@ function App() {
         </Link>
         <nav>
           <Link to="/About">About</Link>
+          <Link to="/Vans">Vans</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/About" element={<About />}></Route>
+        <Route path="/Vans" element={<Vans />}></Route>
       </Routes>
     </BrowserRouter>
   );
